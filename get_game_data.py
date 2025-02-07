9ef43e04ad8bf771ca3d58483aa3d82076e3b1bf
Hayden <haydenc9898@gmail.com>
2025-02-07 00:21:06 -0500

    Update: Able to grab arguments from CLI

--- get_game_data.py
+++ get_game_data.py
@@ -1,1 +1,16 @@
+import os 
+import json
+import shutil
+from subprocess import PIPE, run
+import sys
 
+def main(source, target):
+    pass
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) != 3:
+        raise Exception("You must pass a source and target directory - only")
+    
+    source, target = args[1:]
+    main(source,target)
